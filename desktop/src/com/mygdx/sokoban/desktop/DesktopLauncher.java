d1cf8df6b8d33908ff4538ce8a3487b2f2b208be
ciemnamateria <ciemnamateria96@gmail.com>
2016-08-29 20:19:21 +0200

    first commit

---
+++ desktop/src/com/mygdx/sokoban/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.sokoban.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.sokoban.Sokoban;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sokoban(), config);
+	}
+}
